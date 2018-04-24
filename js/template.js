1b0ee33f68b8c62e0736e1243f68acf2d421ee9c
ctttai <1062155964@qq.com>
2018-04-24 10:04:22 +0800

    Initial commit

---
+++ js/template.js
@@ -0,0 +1,7 @@
+/*
+ * home-head
+ */
+$(function(){
+	head ="<nav class='navbar navbar-default' role='navigation' ><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='#'>Meng Team</a></div><div id='head-icon' class='pull-right'><button type='button' class='btn btn-default btn-lg icon-background' ><span class='glyphicon glyphicon-comment'></span><span class='badge '>1</span></button><button type='button' class='btn btn-default btn-lg icon-background' ><span class='glyphicon glyphicon-envelope'></span><span class='badge' >1</span></button><button type='button' class='btn btn-default btn-lg icon-background' ><span class='glyphicon glyphicon-user'></span><span class='badge' >2</span></button></div></div></nav>";
+	$("#head").html(head);
+})
